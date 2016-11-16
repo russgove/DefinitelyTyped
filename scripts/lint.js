1f397d3431d2656f5a38360e1a499dd1d2ecaf87
Andy Hanson <anhans@microsoft.com>
2016-11-16 12:35:45 -0800

    WIP

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,7 +1,7 @@
 // Usage: npm run lint -- my-package-name
 const pkg = process.argv[2];
 const execSync = require("child_process").execSync;
-const cmd = `node node_modules/tslint/lib/tslint-cli --format stylish ${pkg}/**/*.d.ts`;
+const cmd = `node node_modules/types-publisher/node_modules/tslint/lib/tslint-cli --format stylish ${pkg}/**/*.d.ts`;
 console.log(cmd);
 try {
     // Child process writes directly to our own stdout
